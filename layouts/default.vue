3684dc0c64a244eb7a849e71e8ed6fbb82f54901
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-26 12:44:38 +0300

    [Create] Default Layout

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<script setup>
+const langs = [
+  {
+    name: "Türkçe",
+    value: "tr",
+  },
+  {
+    name: "English",
+    value: "eng",
+  },
+];
+useHead({
+  title: "Display Data App",
+  meta: [
+    {
+      name: "description",
+      content: "Nuxt 3 App with TailwindCSS",
+    },
+  ],
+});
+function langChanged(val) {
+  console.log(val);
+}
+function logOut() {
+  console.log("logOut button clicked");
+}
+</script>
+
+<template>
+  <div>
+    <HeaderComp
+      title="Display Data App"
+      :langs="langs"
+      @lang-changed="langChanged"
+      :main-button-show="true"
+      main-button-name="Log Out"
+      @main-button-clicked="logOut"
+    ></HeaderComp>
+
+    <div class="max-w-7xl mx-auto my-4 border-2 rounded px-2">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
