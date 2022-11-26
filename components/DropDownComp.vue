3e7d0aaaab6e103997285128c8104a9489c34acb
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-26 12:42:59 +0300

    [Create] DropDown Component

---
+++ components/DropDownComp.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+  options: {
+    type: Array,
+    default: [
+      {
+        name: "default option",
+        value: "default",
+      },
+    ],
+  },
+  id: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["valueChanged"]);
+</script>
+
+<template>
+  <select
+    :name="props.name"
+    :id="props.id"
+    @change="
+      (e) => {
+        emit('valueChanged', e.target.value);
+      }
+    "
+    class="p-2 rounded border hover:border-black cursor-pointer"
+  >
+    <option
+      v-for="(option, index) in options"
+      :key="index"
+      :value="option.value"
+    >
+      {{ option.name }}
+    </option>
+  </select>
+</template>
