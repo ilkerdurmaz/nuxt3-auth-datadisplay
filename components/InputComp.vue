b41dde874632c8e19d97e4fb1a5dde8504a3731e
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-26 15:57:44 +0300

    [Create] Input Component

---
+++ components/InputComp.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    autocomplete="off"
+    class="border-2 p-2"
+  />
+</template>
