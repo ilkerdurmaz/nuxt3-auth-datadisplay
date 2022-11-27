606fcff2189202649104cf1264acd77ff04f8fe9
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-27 16:17:52 +0300

    [Update] Auth-Check middleware
    -removed console message

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -1,8 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const auth = useAuth();
-  console.log("auth", auth.isAuth.value.status);
   if (auth.isAuth.value.status) {
-    return true
+    return true;
   }
-  return navigateTo('/login');
+  return navigateTo("/login");
 });
