d3b57435f5b4a55a35bfd75cfb370c80ec3c9260
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-27 11:51:50 +0300

    [Add] i18n integration
    -Vue i18n library installed
    -Configured for Nuxt 3 (plugin)
    -Created needed language json files

---
+++ plugins/i18n.js
@@ -0,0 +1,16 @@
+import { createI18n } from "vue-i18n";
+import en from "../locales/en.json";
+import tr from "../locales/tr.json";
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  const i18n = createI18n({
+    locale: "en",
+    fallbackLocale: "en",
+    globalInjection: true,
+    messages: {
+      en,
+      tr,
+    },
+  });
+  vueApp.use(i18n);
+});
