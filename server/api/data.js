5c1365565f4ece3306626181a3a403943eb18895
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-27 22:16:59 +0300

    [Update] Data API
    -updated for paginated data

--- server/api/data.js
+++ server/api/data.js
@@ -1,10 +1,23 @@
 import getData from "../../services/apiService";
 
+function paginate(arr, size) {
+  return arr.reduce((acc, value, index) => {
+    let idx = Math.floor(index / size);
+    let page = acc[idx] || (acc[idx] = []);
+    page.push(value);
+    return acc;
+  }, []);
+}
+
 export default defineEventHandler(async (event) => {
   const data = await getData("data");
+  const { page, size } = await getQuery(event);
+
+  const paginatedData = paginate(data, size);
 
   return {
     status: 200,
-    data,
+    data: paginatedData[page],
+    pageAmount: paginatedData.length,
   };
 });
