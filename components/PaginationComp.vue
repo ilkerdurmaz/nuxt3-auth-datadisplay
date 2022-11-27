26a7c3f2040a2e6e88e86f2d4e1c67825af64958
ilkerdurmaz <ilker.prime@gmail.com>
2022-11-27 22:15:44 +0300

    [Create] Pagination Component

---
+++ components/PaginationComp.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps(["pageAmount", "selectedPage"]);
+const emit = defineEmits(["buttonClicked"]);
+function clickHandler(val) {
+  emit("buttonClicked", val);
+}
+</script>
+
+<template>
+  <nav class="inline-flex">
+    <ul>
+      <li>
+        <button
+          @click="clickHandler('previous')"
+          class="w-20"
+        >
+          {{ $t("previous") }}
+        </button>
+      </li>
+      <li
+        v-for="(pageNumber, index) in pageAmount"
+        :key="index"
+      >
+        <button @click="clickHandler(pageNumber)">{{ pageNumber }}</button>
+      </li>
+      <li>
+        <button
+          @click="clickHandler('next')"
+          class="w-20"
+        >
+          {{ $t("next") }}
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+<style scoped>
+ul {
+  @apply flex divide-x-2 border-2 border-gray-400 rounded-lg overflow-hidden;
+}
+li {
+  @apply flex justify-center hover:bg-gray-200;
+}
+button {
+  @apply p-3;
+}
+</style>
